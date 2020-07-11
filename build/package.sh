e0006f9e47594e9c3c265d14b1b16373b71ab83f
chenzhiq <chenzhiq@authine.com>
2020-07-11 20:06:59 +0800

    build: 更新package.sh

--- build/package.sh
+++ build/package.sh
@@ -1,22 +1,20 @@
+PATH=/root/tools/node-v10.9.0-linux-x64/bin:/root/tools/node-v10.9.0-linux-x64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin:/usr/local/git_home/git-2.7.2:/usr/local/git/bin:/usr/local/git/bin:/root/bin
+
 # 打印当前的工作目录(Jenkins)
 echo "当前的工作目录：${WORKSPACE}"
 # 进入到工作目录
 cd ${WORKSPACE}
-
-# 切换分支
+# git checkout -b master origin/master
 git checkout master
-#  强行拉取远程分支覆盖本地分支
 git fetch --all
 git reset --hard origin/master
 
-# lerna 初始化模块依赖
+
 echo "执行 lerna bootstrap"
 npm run bootstrap
 
 # 缓存当前项目分支 lerna 模块是否有变动
 lernaChanged=$(npm run changed | tail -n 1)
-
-
 
 hasLernaChanged() {
   # 若是 lerna change 没有变动则退出
@@ -28,13 +26,11 @@ hasLernaChanged() {
   # 0 = true
   return 0
 }
-
 reSignTag() {
   lastTagName=$(git tag | tail -n 1)
   git tag -d $lastTagName
   git tag -a $lastTagName -m "$lastTagName"
 }
-
 
 if hasLernaChanged; then
   echo "lerna 有变动，继续执行"
@@ -50,4 +46,3 @@ fi
 
 # 普通构建任务也会推送到构建
 echo "执行 Jenkins构建概要推送钉钉任务"
-#npm run jk2dt
